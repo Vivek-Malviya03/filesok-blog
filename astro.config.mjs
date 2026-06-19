236f5f1acece6f0ad829400f85832bbbeedb4336
Vivek-Malviya03 <vivekmalviya350@gmail.com>
2026-06-19 17:47:01 +0530

    astro.config.mjs

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,6 +2,6 @@ import { defineConfig } from 'astro/config';
 
 export default defineConfig({
   // This tells Astro to prefix all assets with /blog
-  base: '/blog', 
+   
   // ... rest of your config
 });
