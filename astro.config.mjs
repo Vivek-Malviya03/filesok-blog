c09248b9832617213dace4c1fadd3e7569efb736
Vivek-Malviya03 <vivekmalviya350@gmail.com>
2026-06-19 17:49:28 +0530

    astro.config.mjs

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,7 +1,42 @@
-import { defineConfig } from 'astro/config';
+// @ts-check
 
+import mdx from '@astrojs/mdx';
+import sitemap from '@astrojs/sitemap';
+import { defineConfig, fontProviders } from 'astro/config';
+
+import tailwindcss from '@tailwindcss/vite';
+
+// https://astro.build/config
 export default defineConfig({
-  // This tells Astro to prefix all assets with /blog
-   
-  // ... rest of your config
+  site: 'https://example.com',
+  integrations: [mdx(), sitemap()],
+
+  fonts: [
+      {
+          provider: fontProviders.local(),
+          name: 'Atkinson',
+          cssVariable: '--font-atkinson',
+          fallbacks: ['sans-serif'],
+          options: {
+              variants: [
+                  {
+                      src: ['./src/assets/fonts/atkinson-regular.woff'],
+                      weight: 400,
+                      style: 'normal',
+                      display: 'swap',
+                  },
+                  {
+                      src: ['./src/assets/fonts/atkinson-bold.woff'],
+                      weight: 700,
+                      style: 'normal',
+                      display: 'swap',
+                  },
+              ],
+          },
+      },
+	],
+
+  vite: {
+    plugins: [tailwindcss()],
+  },
 });
