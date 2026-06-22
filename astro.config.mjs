dce8639212546ad9e081be201a4b230f7aba1a42
Vivek Malviya <vivekmalviya2003@Viveks-MacBook-Air.local>
2026-06-22 23:20:03 +0530

    fix: Inline all CSS to fix proxy styling, rebuild all components

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,9 +1,9 @@
 import { defineConfig } from 'astro/config';
-import tailwind from '@astrojs/tailwind';
 
-// https://astro.build/config
 export default defineConfig({
   site: 'https://filesok.com',
-  base: '/blog', // 🚨 THIS IS THE CRITICAL FIX
-  integrations: [tailwind()]
+  output: 'static',
+  build: {
+    inlineStylesheets: 'always',
+  },
 });
