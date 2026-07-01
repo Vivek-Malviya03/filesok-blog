cac246a8838cae04881491c443c6865ad5b92672
Vivek Malviya <vivekmalviya2003@Viveks-MacBook-Air.local>
2026-07-02 00:36:28 +0530

    feat: configure /blog base path and sitemap integration

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,9 +1,12 @@
 import { defineConfig } from 'astro/config';
+import sitemap from '@astrojs/sitemap';
 
 export default defineConfig({
   site: 'https://filesok.com',
+  base: '/blog', // 🚨 Routes everything to the /blog sub-path
   output: 'static',
   build: {
     inlineStylesheets: 'always',
   },
+  integrations: [sitemap()], // 🚨 Automatically generates your sitemap
 });
